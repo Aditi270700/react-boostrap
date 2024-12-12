38717a5f46c34969a68e73255a23055bb7f841c6
aditi saudagar <aditisaudagar17@gmail.com>
2024-12-12 12:32:00 +0530

    react_bootstrap

---
+++ react_bootstrap/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Bootcom from "./Bootcom"
+function App() {
+  
+
+  return (
+    <>
+<Bootcom/>
+      </>
+  )
+}
+export default App
---
+++ react_bootstrap/src/Bootcom.jsx
@@ -0,0 +1,23 @@
+const Bootcom = ()=>{
+    return(
+        <>
+        <div className="=container">
+            <div className="row">
+                <div className="col-lg-6 col-md-8 col-12 bg-primary"><h1>aditi</h1></div>
+                <div className="col-lg-6 col-md-4 col-12 bg-success">lorem epsum</div>
+            </div>
+        </div>
+
+        <div className="card" style={{width: "18rem"}}>
+  <img src="talent.png" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+        </>
+    )
+}
+export default Bootcom
